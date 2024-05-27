77bd4411e199577a14ffd1eb33ebd7b0ea7c517b
jam peng <pengzhe.info@gmail.com>
2024-05-27 23:33:39 +0800

    加入專案檔案。

---
+++ donetCore_beginner/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using donetCore_beginner.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace donetCore_beginner.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ donetCore_beginner/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace donetCore_beginner.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
